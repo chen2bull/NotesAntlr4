b57d0e8d1ee79cbcc7ed5cd4231f5e06a2786b22
chenmingjian <cmingjian@qq.com>
2016-05-12 18:11:07 +0800

    属性文件阅读器

--- src/listeners/PropertyFileListener.java
+++ src/listeners/PropertyFileListener.java
@@ -1,17 +1,29 @@
-/***
- * Excerpted from "The Definitive ANTLR 4 Reference",
- * published by The Pragmatic Bookshelf.
- * Copyrights apply to this code. It may not be used to create training material, 
- * courses, books, articles, and the like. Contact us if you are in doubt.
- * We make no guarantees that this code is fit for any purpose. 
- * Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book information.
-***/
-import org.antlr.v4.runtime.tree.*;
-import org.antlr.v4.runtime.Token;
+// Generated from PropertyFile.g4 by ANTLR 4.5.3
+import org.antlr.v4.runtime.tree.ParseTreeListener;
 
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link PropertyFileParser}.
+ */
 public interface PropertyFileListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link PropertyFileParser#file}.
+	 * @param ctx the parse tree
+	 */
 	void enterFile(PropertyFileParser.FileContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PropertyFileParser#file}.
+	 * @param ctx the parse tree
+	 */
 	void exitFile(PropertyFileParser.FileContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link PropertyFileParser#prop}.
+	 * @param ctx the parse tree
+	 */
 	void enterProp(PropertyFileParser.PropContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link PropertyFileParser#prop}.
+	 * @param ctx the parse tree
+	 */
 	void exitProp(PropertyFileParser.PropContext ctx);
 }
