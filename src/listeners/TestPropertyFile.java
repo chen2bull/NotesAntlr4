314c62b1cfb3e504a3c53aee4044016ec92ccb31
chenmingjian <cmingjian@qq.com>
2016-05-14 11:51:13 +0800

    CSV文件Loader

--- src/listeners/TestPropertyFile.java
+++ src/listeners/TestPropertyFile.java
@@ -15,7 +15,7 @@ import java.util.Map;
 
 public class TestPropertyFile {
     public static class PropertyFileLoader extends PropertyFileBaseListener {
-        Map<String,String> props = new OrderedHashMap<>();
+        Map<String,String> props = new OrderedHashMap<String, String>();
         public void exitProp(PropertyFileParser.PropContext ctx) {
             String id = ctx.ID().getText(); // prop : ID '=' STRING '\n' ;
             String value = ctx.STRING().getText();
